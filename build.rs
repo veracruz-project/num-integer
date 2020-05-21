423f1f89ec063535e69be58bb17030b05ae3b0d7
Shale Xiong <shale.xiong@arm.com>
2020-05-21 14:35:36 +0100

    switch off mesalock_sgx

--- build.rs
+++ build.rs
@@ -1,14 +1,22 @@
-//extern crate autocfg;
+#[cfg(not(feature = "mesalock_sgx"))]
+extern crate autocfg;
 
-//use std::env;
+#[cfg(not(feature = "mesalock_sgx"))]
+use std::env;
 
+#[cfg(not(feature = "mesalock_sgx"))]
 fn main() {
-    //let ac = autocfg::new();
-    //if ac.probe_type("i128") {
+    let ac = autocfg::new();
+    if ac.probe_type("i128") {
     println!("cargo:rustc-cfg=has_i128");
-    //} else if env::var_os("CARGO_FEATURE_I128").is_some() {
-    //    panic!("i128 support was not detected!");
-    //}
+    } else if env::var_os("CARGO_FEATURE_I128").is_some() {
+        panic!("i128 support was not detected!");
+    }
 
-    //autocfg::rerun_path(file!());
+    autocfg::rerun_path(file!());
+}
+
+#[cfg(feature = "mesalock_sgx")]
+fn main() {
+    println!("cargo:rustc-cfg=has_i128");
 }
